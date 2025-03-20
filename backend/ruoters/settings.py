34bc0aafc412c047b1616ebbc95a7805aec15609
naseh42 <naseh42@gmail.com>
2025-03-20 23:05:38 +0330

    Update settings.py

--- backend/ruoters/settings.py
+++ backend/ruoters/settings.py
@@ -1,13 +1,1 @@
-from fastapi import APIRouter
 
-router = APIRouter()
-
-# دریافت تنظیمات
-@router.get("/")
-def get_settings():
-    return {"message": "Settings data"}
-
-# به‌روزرسانی تنظیمات
-@router.put("/")
-def update_settings():
-    return {"message": "Settings updated successfully"}
